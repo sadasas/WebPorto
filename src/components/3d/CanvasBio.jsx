18ed1714878db9500dd4ce6ace10dcb43eccbaca
wahyusrp <setio101199@gmail.com>
2023-06-07 08:26:46 +0700

    fix

--- src/components/3d/CanvasBio.jsx
+++ src/components/3d/CanvasBio.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 
 import { Character3D } from "./Character3D";
+import DotLoader from "@components/loaders/DotLoader";
 import styles from "@styles/canvas/canvasBio.module.scss";
 
 function CanvasBio() {
   return (
     <div className={styles["container"]}>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[0, 0, 0]} />
-        <Character3D position={[0, -2, 0]} />
-      </Canvas>
+      <Suspense fallback={<DotLoader />}>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[0, 0, 0]} />
+          <Character3D position={[0, -2, 0]} />
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,9 @@
-import React, { Suspense } from "react";
+import React from "react";
 
 import styles from "@styles/about/About.module.scss";
 import Biography from "./Biography";
 import MySkill from "./MySkill";
-import DotLoader from "@components/loaders/DotLoader";
+
 const CanvasBio = React.lazy(() => import("@components/3d/CanvasBio"));
 
 function About() {
@@ -11,9 +11,7 @@ function About() {
     <section id="about" className={styles["container"]}>
       <div className={styles["about-container"]}>
         <div className={styles["bio-container"]}>
-          <Suspense fallback={<DotLoader />}>
-            <CanvasBio />
-          </Suspense>
+          <CanvasBio />
           <Biography />
         </div>
         <MySkill />
